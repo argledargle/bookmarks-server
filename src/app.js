de0896b9557b8311b2a92d244ac20f891294bb66
Alex Peter <apyells@gmail.com>
2019-07-17 10:03:22 -0500

    finalizes all GET, POST, DELETE functionality

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  const authToken = req.get("Authorization");
+  const authToken = req.headers.authorization;
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`)
     return res.status(401).json({ error: "Unauthorized request" });
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,26 +1,69 @@
 const express = require("express");
+const { bookmarks } = require('../store');
+const uuid = require("uuid/v4");
 const logger = require("../logger");
 
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 
+
+
 bookmarkRouter
   .route("/bookmarks")
   .get((req, res) => {
-    //Write a route handler for the endpoint GET /bookmarks that
-    //returns a list of bookmarks
+    res.json(bookmarks)
   })
   .post(bodyParser, (req, res) => {
-    //Write a route handler for POST /bookmarks that accepts a 
-    //JSON object representing a bookmark and adds it to the list 
-    //of bookmarks after validation
+      const { title, url, description, rating } = req.body;
+      const id = uuid();
+      if(!title) {
+          logger.error(`Title is required`);
+          return res.status(400).send("Invalid data");
+      };
+      if(!url) {
+          logger.error(`URL is required`);
+          return res.status(400).send("Invalid data");
+      };
+      if(!description) {
+          logger.error(`Description is required`);
+          return res.status(400).send("Invalid data");
+      };
+      if(!rating) {
+          logger.error(`Rating is required`);
+          return res.status(400).send("Invalid data");
+      };
+      const bookmark = {
+          id,
+          title,
+          url,
+          description,
+          rating
+      };
+      bookmarks.push(bookmark);
+      logger.info(`Bookmark with ${id} created.`);
+      res.status(201).location(`http://localhost:800/bookmarks/${id}`).json(bookmark)
   });
 
 bookmarkRouter.route("/bookmarks/:id").get((req, res) => {
-    //Write a route handler for the endpoint GET /bookmarks/:id 
-    //that returns a single bookmark with the given ID, return 
-    //404 Not Found if the ID is not valid
+    const { id } = req.params;
+    const bookmark = bookmarks.find(b => b.id == id);
+    if (!bookmark) {
+        logger.error(`Bookmark with id ${id} not found.`);
+        return res.status(400).send("Bookmark Not Found");
+    }
+    res.json(bookmark);
 }).delete((req, res) => {
+    const { id } = req.params;
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+    if (bookmarkIndex === -1) {
+        logger.error(`Bookmark with id ${id} not found.`);
+        return res(404).send("Not found")
+    }
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.`)
+
+    res.status(204).end();
     //Write a route handler for the endpoint DELETE /bookmarks/:id
     //that deletes the bookmark with the given ID
 })
